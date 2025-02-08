0309dcb6fe68a14154833d253741e4f07a299c63
satyajit.dotlinkertech <satyajit@dotlinkertech.com>
2025-02-08 18:48:45 +0530

    add htaccess file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
-import routes from "tempo-routes";
 import Blog from "./components/blog";
 import Contact from "./components/contact";
 import About from "./components/about";
@@ -9,17 +8,15 @@ import About from "./components/about";
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blogs" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
-      </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Suspense>
   );
 }
 
 export default App;
+
